c6d48a9744f47a9e4fba9a0190485af58996f36d
lumiaen <lumiaen@gmail.com>
2014-11-17 20:52:49 +0200

    RC Car works, untested speed setting.

--- app/src/main/java/com/coolandkasparof/androidbluetootharduino/MainActivity.java
+++ app/src/main/java/com/coolandkasparof/androidbluetootharduino/MainActivity.java
@@ -3,11 +3,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
 import app.akexorcist.bluetoothspp.BluetoothSPP;
 import app.akexorcist.bluetoothspp.BluetoothSPP.BluetoothConnectionListener;
 import app.akexorcist.bluetoothspp.BluetoothSPP.OnDataReceivedListener;
@@ -17,7 +23,7 @@ import app.akexorcist.bluetoothspp.DeviceList;
 
 
 public class MainActivity extends Activity {
-
+    public String speed="150";
     BluetoothSPP bt;
     private TextView text;
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,16 +90,113 @@ public class MainActivity extends Activity {
     }
 
     public void setup() {
-        Button btnTemp = (Button)findViewById(R.id.Temp);
-        btnTemp.setOnClickListener(new OnClickListener(){
-            public void onClick(View v){
-                bt.send("0", true);
+        final Button fwd = (Button)findViewById(R.id.forward);
+        fwd.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View fwd, MotionEvent theMotion) {
+
+                switch ( theMotion.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        bt.send("*f"+speed, false);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bt.send("*f000", false);
+                        break;
+                }
+                return true;
             }
         });
-        Button  btnHumid = (Button)findViewById(R.id.Humid);
-        btnHumid.setOnClickListener(new OnClickListener(){
-            public void onClick(View v){
-                bt.send("1", true);
+
+
+       final Button  bck = (Button)findViewById(R.id.back);
+        bck.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View bck, MotionEvent theMotion) {
+
+                switch (theMotion.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        bt.send("*b"+speed, false);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bt.send("*b000", false);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        final Button lft = (Button)findViewById(R.id.left);
+        lft.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View lft, MotionEvent theMotion) {
+
+                switch ( theMotion.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        bt.send("*l", false);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bt.send("*s", false);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        final Button rgt = (Button)findViewById(R.id.right);
+        rgt.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View rgt, MotionEvent theMotion) {
+
+                switch ( theMotion.getAction() ) {
+                    case MotionEvent.ACTION_DOWN:
+                        bt.send("*r", false);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        bt.send("*s", false);
+                        break;
+                }
+                return true;
+            }
+        });
+        final CheckBox low = (CheckBox)findViewById(R.id.low);
+        final CheckBox medium = (CheckBox)findViewById(R.id.medium);
+        final CheckBox high = (CheckBox)findViewById(R.id.high);
+        low.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton low, boolean isChecked) {
+            if (isChecked){
+                medium.setChecked(false);
+                high.setChecked(false);
+                speed="150";
+            }else{
+                if (!medium.isChecked()&&!high.isChecked()){
+                    low.setChecked(true);
+                    speed="150";
+                }
+            }
+            }
+        });
+        medium.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton medium, boolean isChecked) {
+                if (isChecked){
+                    low.setChecked(false);
+                    high.setChecked(false);
+                    speed="200";
+                }else{
+                    if (!low.isChecked()&&!high.isChecked()){
+                        medium.setChecked(true);
+                        speed="200";
+                    }
+                }
+            }
+        });
+        high.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton high, boolean isChecked) {
+                if (isChecked){
+                    medium.setChecked(false);
+                    low.setChecked(false);
+                    speed="255";
+                }else{
+                    if (!medium.isChecked()&&!low.isChecked()){
+                        high.setChecked(true);
+                        speed="255";
+                    }
+                }
             }
         });
     }
